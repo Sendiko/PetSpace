8f871ab397c4dc938c7b24f5a828b109b99c5500
Sendiko <3103120150@student.smktelkom-pwt.sch.id>
2022-12-02 00:34:40 +0700

    new components
    added signup and signin screens

--- app/src/main/java/com/sendiko/petspace/ui/navigaton/NavGraph.kt
+++ app/src/main/java/com/sendiko/petspace/ui/navigaton/NavGraph.kt
@@ -4,12 +4,13 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import com.sendiko.petspace.ui.screens.SignInScreen
+import com.sendiko.petspace.ui.screens.SignUpScreen
 import com.sendiko.petspace.ui.screens.WelcomeScreen
 
 @Composable
 fun SetupNavGraph(
     navController: NavHostController,
-
 ) {
     NavHost(
         navController = navController,
@@ -18,7 +19,23 @@ fun SetupNavGraph(
         composable(
             route = Screens.WelcomeScreen.route
         ){
-            WelcomeScreen()
+            WelcomeScreen(
+                navController = navController
+            )
+        }
+        composable(
+            route = Screens.SignUpScreen.route
+        ){
+            SignUpScreen(
+                navController = navController
+            )
+        }
+        composable(
+            route = Screens.SignInScreen.route
+        ){
+            SignInScreen(
+                navController = navController
+            )
         }
     }
 }
