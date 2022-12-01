5e98fd5446270434d5ac14c19c3999d2b83d062b
Sendiko <3103120150@student.smktelkom-pwt.sch.id>
2022-12-02 00:47:45 +0700

    update component
    add topbar

--- app/src/main/java/com/sendiko/petspace/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/sendiko/petspace/ui/screens/HomeScreen.kt
@@ -1,8 +1,24 @@
 package com.sendiko.petspace.ui.screens
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
+import com.sendiko.petspace.R
 import com.sendiko.petspace.ui.theme.darkBlue
 
 @Composable
@@ -10,7 +26,33 @@ fun HomeScreen(
     navController: NavHostController
 ) {
     Scaffold(
-        backgroundColor = darkBlue
+        backgroundColor = darkBlue,
+        topBar = {
+            TopAppBar(
+                backgroundColor = darkBlue,
+                contentPadding = PaddingValues(horizontal = 16.dp)
+            ) {
+                Column(
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.Start
+                ) {
+                    Text(
+                        text = "Hi, Sendiko!",
+                        style = TextStyle(
+                            color = Color.White,
+                            fontSize = 24.sp
+                        )
+                    )
+                    Text(
+                        text = "Looking for soulmate?",
+                        style = TextStyle(
+                            color = Color.White,
+                            fontSize = 18.sp
+                        )
+                    )
+                }
+            }
+        }
     ) {
 
     }
