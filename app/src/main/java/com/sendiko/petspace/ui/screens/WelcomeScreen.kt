cca17be33b7f33f2b726ab1abf49b68a26de0020
Sendiko <3103120150@student.smktelkom-pwt.sch.id>
2022-12-01 23:18:20 +0700

    new component
    finish adding welcome screen

---
+++ app/src/main/java/com/sendiko/petspace/ui/screens/WelcomeScreen.kt
@@ -0,0 +1,91 @@
+package com.sendiko.petspace.ui.screens
+
+import android.annotation.SuppressLint
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.sendiko.petspace.R
+import com.sendiko.petspace.ui.theme.cyan
+import com.sendiko.petspace.ui.theme.darkBlue
+
+@SuppressLint("UnusedMaterialScaffoldPaddingParameter")
+@Composable
+fun WelcomeScreen() {
+    Scaffold(
+        backgroundColor = darkBlue
+    ) {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.SpaceBetween,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Column(
+                modifier = Modifier.padding(top = 256.dp),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Image(
+                    modifier = Modifier.size(128.dp),
+                    imageVector = ImageVector.vectorResource(id = R.drawable.petspace),
+                    contentDescription = null
+                )
+                Text(
+                    text = "PetSpace",
+                    style = TextStyle(
+                        color = Color.White,
+                        fontSize = 24.sp
+                    )
+                )
+            }
+            Column(
+                modifier = Modifier.fillMaxWidth(),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Button(
+                    onClick = { /*TODO*/ },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 16.dp, vertical = 4.dp)
+                        .height(64.dp),
+                    colors = ButtonDefaults.buttonColors(cyan)
+                ) {
+                    Text(
+                        "Sign Up",
+                        style = TextStyle(
+                            fontSize = 18.sp,
+                            color = Color.White
+                        )
+                    )
+                }
+                OutlinedButton(
+                    onClick = { /*TODO*/ },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 16.dp, vertical = 8.dp)
+                        .height(64.dp),
+                    border = BorderStroke(color = cyan, width = 2.dp),
+                    colors = ButtonDefaults.buttonColors(darkBlue)
+                ) {
+                    Text(
+                        "Sign In",
+                        style = TextStyle(
+                            fontSize = 18.sp,
+                            color = Color.White
+                        )
+                    )
+                }
+            }
+        }
+    }
+}
