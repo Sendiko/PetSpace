31b589de2cf7543611dd8947074855f227f46974
Sendiko <3103120150@student.smktelkom-pwt.sch.id>
2022-12-02 08:28:44 +0700

    update component
    add profile pic

--- app/src/main/java/com/sendiko/petspace/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/sendiko/petspace/ui/screens/HomeScreen.kt
@@ -1,22 +1,26 @@
 package com.sendiko.petspace.ui.screens
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
+import com.sendiko.petspace.R
 import com.sendiko.petspace.repository.PetRepository
 import com.sendiko.petspace.ui.component.PetCard
 import com.sendiko.petspace.ui.theme.darkBlue
@@ -31,25 +35,41 @@ fun HomeScreen(
         topBar = {
             TopAppBar(
                 backgroundColor = darkBlue,
-                contentPadding = PaddingValues(horizontal = 16.dp)
+                contentPadding = PaddingValues(horizontal = 16.dp, vertical = 8.dp)
             ) {
-                Column(
-                    verticalArrangement = Arrangement.Center,
-                    horizontalAlignment = Alignment.Start
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                    verticalAlignment = Alignment.CenterVertically
                 ) {
-                    Text(
-                        text = "Hi, Sendiko!",
-                        style = TextStyle(
-                            color = Color.White,
-                            fontSize = 24.sp
+                    Column(
+                        verticalArrangement = Arrangement.Center,
+                        horizontalAlignment = Alignment.Start
+                    ) {
+                        Text(
+                            text = "Hi, Sendiko!",
+                            style = TextStyle(
+                                color = Color.White,
+                                fontSize = 24.sp
+                            )
                         )
-                    )
-                    Text(
-                        text = "Looking for soulmate?",
-                        style = TextStyle(
-                            color = Color.White,
-                            fontSize = 18.sp
+                        Text(
+                            text = "Looking for soulmate?",
+                            style = TextStyle(
+                                color = Color.White,
+                                fontSize = 18.sp
+                            )
                         )
+                    }
+                    Image(
+                        alignment = Alignment.Center,
+                        contentScale = ContentScale.Fit,
+                        painter = painterResource(id = R.drawable.profile_photo),
+                        contentDescription = "Image Header",
+                        modifier = Modifier
+                            .size(48.dp)
+                            .clip(RoundedCornerShape(8.dp))
+                            .clickable {  },
                     )
                 }
             }
