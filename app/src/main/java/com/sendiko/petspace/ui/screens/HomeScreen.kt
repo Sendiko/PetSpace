97d17d8322b00c5eee718a16071057e1f815013d
Sendiko <3103120150@student.smktelkom-pwt.sch.id>
2022-12-02 08:15:53 +0700

    update components
    add lazy column items

--- app/src/main/java/com/sendiko/petspace/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/sendiko/petspace/ui/screens/HomeScreen.kt
@@ -1,10 +1,11 @@
 package com.sendiko.petspace.ui.screens
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
@@ -12,19 +13,19 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
-import com.sendiko.petspace.R
+import com.sendiko.petspace.repository.PetRepository
+import com.sendiko.petspace.ui.component.PetCard
 import com.sendiko.petspace.ui.theme.darkBlue
 
 @Composable
 fun HomeScreen(
-    navController: NavHostController
+    navController: NavHostController,
 ) {
+    val petData = PetRepository().getAllPet()
     Scaffold(
         backgroundColor = darkBlue,
         topBar = {
@@ -54,6 +55,17 @@ fun HomeScreen(
             }
         }
     ) {
-
+        LazyColumn(
+            Modifier.padding(16.dp),
+            verticalArrangement = Arrangement.spacedBy(8.dp)
+        ){
+            items(
+                items = petData
+            ){ pet ->
+                PetCard(
+                    pet = pet,
+                )
+            }
+        }
     }
 }
