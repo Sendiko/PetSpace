9d58a374e4610b391976855245fe33fb44484d48
Sendiko <3103120150@student.smktelkom-pwt.sch.id>
2022-12-03 06:38:52 +0700

    new component
    add custom topappbar

--- app/src/main/java/com/sendiko/petspace/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/sendiko/petspace/ui/screens/HomeScreen.kt
@@ -1,32 +1,25 @@
 package com.sendiko.petspace.ui.screens
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Scaffold
-import androidx.compose.material.Text
-import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import com.sendiko.petspace.R
 import com.sendiko.petspace.repository.PetRepository
 import com.sendiko.petspace.repository.viewmodel.PetViewModel
 import com.sendiko.petspace.ui.component.PetCard
+import com.sendiko.petspace.ui.component.TopAppBarWithSubtitle
 import com.sendiko.petspace.ui.theme.darkBlue
-import com.sendiko.petspace.ui.theme.poppinsFamily
 
 @Composable
 fun HomeScreen(
@@ -37,50 +30,16 @@ fun HomeScreen(
     Scaffold(
         backgroundColor = darkBlue,
         topBar = {
-            TopAppBar(
+            TopAppBarWithSubtitle(
                 backgroundColor = darkBlue,
-                contentPadding = PaddingValues(horizontal = 16.dp, vertical = 8.dp)
-            ) {
-                Row(
-                    modifier = Modifier.fillMaxWidth(),
-                    horizontalArrangement = Arrangement.SpaceBetween,
-                    verticalAlignment = Alignment.CenterVertically
-                ) {
-                    Column(
-                        verticalArrangement = Arrangement.Center,
-                        horizontalAlignment = Alignment.Start
-                    ) {
-                        Text(
-                            text = "Hi, Sendiko!",
-                            style = TextStyle(
-                                color = Color.White,
-                                fontSize = 20.sp,
-                                fontFamily = poppinsFamily,
-                                fontWeight = FontWeight.SemiBold,
-                            )
-                        )
-                        Text(
-                            text = "Adopt a new friend near you!",
-                            style = TextStyle(
-                                color = Color.White,
-                                fontSize = 14.sp,
-                                fontFamily = poppinsFamily,
-                                fontWeight = FontWeight.Normal,
-                            )
-                        )
-                    }
-                    Image(
-                        alignment = Alignment.Center,
-                        contentScale = ContentScale.Fit,
-                        painter = painterResource(id = R.drawable.profile_photo),
-                        contentDescription = "Image Header",
-                        modifier = Modifier
-                            .size(48.dp)
-                            .clip(RoundedCornerShape(8.dp))
-                            .clickable {  },
-                    )
-                }
-            }
+                title = "Hi, Sendiko!",
+                subtitle = "Adopt new friend near you!",
+                image = painterResource(id = R.drawable.profile_photo),
+                modifier = Modifier
+                    .size(48.dp)
+                    .clip(RoundedCornerShape(8.dp))
+                    .clickable { }
+            )
         }
     ) {
         LazyColumn(
--- app/src/main/java/com/sendiko/petspace/ui/screens/SignInScreen.kt
+++ app/src/main/java/com/sendiko/petspace/ui/screens/SignInScreen.kt
@@ -1,25 +1,23 @@
 package com.sendiko.petspace.ui.screens
 
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Scaffold
-import androidx.compose.material.Text
-import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import com.sendiko.petspace.ui.component.CustomTextField
 import com.sendiko.petspace.ui.component.LargeSolidButton
+import com.sendiko.petspace.ui.component.RegularTopAppBar
 import com.sendiko.petspace.ui.navigaton.Screens
 import com.sendiko.petspace.ui.theme.cyan
 import com.sendiko.petspace.ui.theme.darkBlue
-import com.sendiko.petspace.ui.theme.poppinsFamily
 
 @Composable
 fun SignInScreen(
@@ -35,21 +33,10 @@ fun SignInScreen(
     Scaffold(
         backgroundColor = darkBlue,
         topBar = {
-            TopAppBar(
+            RegularTopAppBar(
                 backgroundColor = darkBlue,
-                elevation = 0.dp,
-                contentPadding = PaddingValues(horizontal = 16.dp)
-            ) {
-                Text(
-                    text = "Sign In",
-                    style = TextStyle(
-                        fontSize = 24.sp,
-                        color = Color.White,
-                        fontFamily = poppinsFamily,
-                        fontWeight = FontWeight.Bold,
-                    )
-                )
-            }
+                title = "Sign In"
+            )
         }
     ) {
         Column(
