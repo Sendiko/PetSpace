b2056b4e711bfd6d452eb60b48b94c0708891eac
Sendiko <3103120150@student.smktelkom-pwt.sch.id>
2022-12-02 08:14:32 +0700

    update component
    new colors

--- app/src/main/java/com/sendiko/petspace/ui/theme/Color.kt
+++ app/src/main/java/com/sendiko/petspace/ui/theme/Color.kt
@@ -9,5 +9,7 @@ val Teal200 = Color(0xFF03DAC5)
 
 val darkBlue = Color(0xFF0C1B3A)
 val blue = Color(0xFF162544)
+val blueAlternative = Color(0xFF162C53)
+val red = Color(0xFFD7060B)
 val lightBlue = Color(0xFF0275B3)
 val cyan = Color(0xFF147EFF)
