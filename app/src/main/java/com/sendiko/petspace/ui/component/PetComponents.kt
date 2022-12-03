60cb8bf9d267bac5896e18144fe9141a918c350e
Sendiko <3103120150@student.smktelkom-pwt.sch.id>
2022-12-03 07:16:14 +0700

    update component
    pet detail screen is done

--- app/src/main/java/com/sendiko/petspace/ui/component/PetComponents.kt
+++ app/src/main/java/com/sendiko/petspace/ui/component/PetComponents.kt
@@ -10,19 +10,23 @@ import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Email
 import androidx.compose.material.icons.filled.LocationOn
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
+import com.sendiko.petspace.R
 import com.sendiko.petspace.repository.model.Pet
 import com.sendiko.petspace.repository.viewmodel.PetViewModel
 import com.sendiko.petspace.ui.navigaton.Screens
@@ -88,11 +92,11 @@ fun PetCard(
                 ) {
                     Text(
                         modifier = Modifier
-                            .padding(start = 8.dp),
+                            .padding(start = 16.dp),
                         text = pet.name,
                         style = TextStyle(
                             color = Color.White,
-                            fontSize = 24.sp,
+                            fontSize = 20.sp,
                             fontFamily = poppinsFamily,
                             fontWeight = FontWeight.Medium
                         )
@@ -179,7 +183,7 @@ fun PetTitle(
 }
 
 @Composable
-fun PetAdress(
+fun PetAddress(
     pet: Pet
 ) {
     Row(
@@ -231,8 +235,154 @@ fun PetStory(
                 color = Color.White,
                 fontFamily = poppinsFamily,
                 fontWeight = FontWeight.Normal,
-                fontSize = 14.sp
+                fontSize = 16.sp
             )
         )
     }
+}
+
+@Composable
+fun PetInfoBox(
+    pet: Pet,
+    info : String
+) {
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = Modifier
+            .background(
+                color = blue,
+                shape = RoundedCornerShape(8.dp)
+            )
+            .height(96.dp)
+            .width(128.dp)
+    ){
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.spacedBy(4.dp),
+        ) {
+            Text(
+                text =
+                when(info){
+                    "Age" -> "${pet.age} years"
+                    "Weight" -> "${pet.weight} Kg"
+                    "Color" -> pet.color
+                    else -> {"null"}
+                },
+                style = TextStyle(
+                    color = Color.White,
+                    fontSize = 18.sp,
+                    fontWeight = FontWeight.Medium
+                )
+            )
+            Text(
+                text = info,
+                style = TextStyle(
+                    color = Color.White,
+                    fontSize = 14.sp,
+                    fontWeight = FontWeight.Normal
+                )
+            )
+        }
+    }
+}
+
+@Composable
+fun PetOwnerInfo(
+    backgroundColor: Color,
+    ownerName : String,
+    image : Painter,
+    modifier: Modifier
+) {
+    Column(
+        modifier = Modifier
+            .padding(horizontal = 16.dp)
+            .background(backgroundColor)
+    ) {
+        Text(
+            text = "Owner Info",
+            style = TextStyle(
+                color = Color.White,
+                fontFamily = poppinsFamily,
+                fontWeight = FontWeight.SemiBold,
+                fontSize = 20.sp
+            )
+        )
+        Row(
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 16.dp)
+        ) {
+            Row(
+                horizontalArrangement = Arrangement.Start,
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier.wrapContentWidth()
+            ) {
+                Image(
+                    alignment = Alignment.Center,
+                    contentScale = ContentScale.Fit,
+                    painter = image,
+                    contentDescription = "Image Header",
+                    modifier = Modifier
+                        .size(64.dp)
+                        .clip(RoundedCornerShape(8.dp)),
+                )
+                Column(
+                    verticalArrangement = Arrangement.SpaceEvenly,
+                    horizontalAlignment = Alignment.Start,
+                    modifier = Modifier
+                        .wrapContentWidth()
+                        .padding(start = 16.dp)
+                ) {
+                    Text(
+                        text = ownerName,
+                        style = TextStyle(
+                            color = Color.White,
+                            fontFamily = poppinsFamily,
+                            fontWeight = FontWeight.Medium,
+                            fontSize = 18.sp
+                        )
+                    )
+                    Text(
+                        text = "Pet Lover",
+                        style = TextStyle(
+                            color = Color.White,
+                            fontFamily = poppinsFamily,
+                            fontWeight = FontWeight.Normal,
+                            fontSize = 16.sp
+                        )
+                    )
+                }
+            }
+            Box(
+                contentAlignment = Alignment.Center,
+                modifier = Modifier
+                    .background(
+                        cyan,
+                        shape = RoundedCornerShape(48.dp)
+                    )
+            ){
+                Icon(
+                    Icons.Filled.Email,
+                    contentDescription = null,
+                    modifier = modifier,
+                    tint = Color.White
+                )
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun PetComponentsPrev() {
+    PetOwnerInfo(
+        backgroundColor = darkBlue,
+        ownerName = "Jennie Kim",
+        image = painterResource(id = R.drawable.profile_photo),
+        modifier = Modifier
+            .size(44.dp)
+            .padding(8.dp)
+    )
 }
--- app/src/main/java/com/sendiko/petspace/ui/screens/PetDetailScreen.kt
+++ app/src/main/java/com/sendiko/petspace/ui/screens/PetDetailScreen.kt
@@ -4,27 +4,25 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
-import androidx.compose.material.Icon
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
-import androidx.compose.material.TopAppBar
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
+import com.sendiko.petspace.R
 import com.sendiko.petspace.repository.viewmodel.PetViewModel
-import com.sendiko.petspace.ui.component.PetAdress
-import com.sendiko.petspace.ui.component.PetImageHeader
-import com.sendiko.petspace.ui.component.PetStory
-import com.sendiko.petspace.ui.component.PetTitle
+import com.sendiko.petspace.ui.component.*
 import com.sendiko.petspace.ui.navigaton.Screens
+import com.sendiko.petspace.ui.theme.cyan
 import com.sendiko.petspace.ui.theme.darkBlue
 import com.sendiko.petspace.ui.theme.poppinsFamily
 
@@ -37,28 +35,16 @@ fun PetDetailScreen(
     Scaffold(
         backgroundColor = darkBlue,
         topBar = {
-            TopAppBar(
+            TopAppBarWithIcon(
                 backgroundColor = darkBlue,
-                elevation = 0.dp,
-                contentPadding = PaddingValues(horizontal = 16.dp),
-            ) {
-                Icon(
-                    Icons.Default.ArrowBack,
-                    contentDescription = "arrow back",
-                    modifier = Modifier.clickable { navController.navigate(Screens.HomeScreen.route) },
-                    tint = Color.White
-                )
-                Spacer(modifier = Modifier.padding(end = 8.dp))
-                Text(
-                    text = "Detail",
-                    style = TextStyle(
-                        fontSize = 22.sp,
-                        color = Color.White,
-                        fontFamily = poppinsFamily,
-                        fontWeight = FontWeight.SemiBold,
-                    )
-                )
-            }
+                icon = Icons.Filled.ArrowBack,
+                iconTint = Color.White,
+                title = "Details",
+                modifier = Modifier.clickable {
+                    navController.popBackStack()
+                    navController.navigate(Screens.HomeScreen.route)
+                }
+            )
         }
     ) {
         LazyColumn {
@@ -71,8 +57,8 @@ fun PetDetailScreen(
             item {
                 PetTitle(pet = pet)
             }
-            item{
-                PetAdress(pet = pet)
+            item {
+                PetAddress(pet = pet)
             }
             item {
                 PetStory(pet = pet)
@@ -93,12 +79,53 @@ fun PetDetailScreen(
                             fontSize = 20.sp
                         )
                     )
-                    LazyRow(
-                        horizontalArrangement = Arrangement.spacedBy(16.dp)
-                    ){
-
+                }
+                Spacer(modifier = Modifier.padding(bottom = 16.dp))
+                LazyRow(
+                    horizontalArrangement = Arrangement.spacedBy(16.dp),
+                    modifier = Modifier.padding(horizontal = 16.dp)
+                ){
+                    item {
+                        PetInfoBox(
+                            pet = pet,
+                            info = "Age"
+                        )
+                    }
+                    item {
+                        PetInfoBox(
+                            pet = pet,
+                            info = "Weight"
+                        )
+                    }
+                    item {
+                        PetInfoBox(
+                            pet = pet,
+                            info = "Color"
+                        )
                     }
                 }
+            }
+            item{
+                Spacer(modifier = Modifier.padding(bottom = 16.dp))
+                PetOwnerInfo(
+                    backgroundColor = darkBlue,
+                    ownerName = pet.previousOwner,
+                    image = painterResource(id = R.drawable.profile_photo),
+                    modifier = Modifier
+                        .size(44.dp)
+                        .padding(8.dp)
+                )
+            }
+            item { 
+                Spacer(modifier = Modifier.padding(bottom = 16.dp))
+                LargeSolidButton(
+                    onClick = { /*TODO*/ },
+                    horizontalPaddingValues = 16,
+                    verticalPaddingValues = 16,
+                    buttonColors = cyan,
+                    buttonText = "Adopt Me",
+                    buttonTextColor = Color.White
+                )
             }
         }
     }
