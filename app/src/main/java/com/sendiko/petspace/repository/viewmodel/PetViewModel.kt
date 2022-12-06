5e9bfcb693162c5f7f691f7e5412de5b7becabb0
Sendiko <sarangtawon897@gmail.com>
2022-12-06 21:50:46 +0700

    repository changes
    little adjustment

--- app/src/main/java/com/sendiko/petspace/repository/viewmodel/PetViewModel.kt
+++ app/src/main/java/com/sendiko/petspace/repository/viewmodel/PetViewModel.kt
@@ -3,14 +3,18 @@ package com.sendiko.petspace.repository.viewmodel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.sendiko.petspace.repository.model.Pet
+import kotlinx.coroutines.launch
 
 class PetViewModel : ViewModel() {
     private val _pet = MutableLiveData<Pet>()
     val pet : LiveData<Pet> = _pet
 
     fun savePet(pet: Pet){
-        _pet.value = pet
+        viewModelScope.launch {
+            _pet.value = pet
+        }
     }
 
 }
