c1fcc3b7561ff1dae2694facc449102fd992aee5
Sendiko <sarangtawon897@gmail.com>
2022-12-09 19:46:31 +0700

    update component
    leave the profile screen like this for now

--- app/src/main/java/com/sendiko/petspace/ui/screens/ProfleScreen.kt
+++ app/src/main/java/com/sendiko/petspace/ui/screens/ProfleScreen.kt
@@ -2,27 +2,31 @@ package com.sendiko.petspace.ui.screens
 
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.navigation.NavHostController
 import com.sendiko.petspace.R
 import com.sendiko.petspace.repository.OwnerRepo
 import com.sendiko.petspace.ui.component.ProfileHeader
-import com.sendiko.petspace.ui.theme.darkBlue
+import com.sendiko.petspace.ui.theme.lightBlueAlternative
+import com.sendiko.petspace.ui.theme.textStyle
 
 @Composable
 fun ProfileScreen(
-    navController: NavHostController
+//    navController: NavHostController
 ) {
     val ownerData = OwnerRepo().getOwnerInfo()
     Scaffold(
-        backgroundColor = darkBlue,
+        backgroundColor = lightBlueAlternative,
         topBar = {
             ProfileHeader(
-                backgroundColor = darkBlue,
+                backgroundColor = lightBlueAlternative,
                 image = painterResource(id = R.drawable.profile_photo),
                 modifier = Modifier,
                 owner = ownerData
@@ -35,18 +39,52 @@ fun ProfileScreen(
             modifier = Modifier.fillMaxSize()
         ){
             Column(
-                Modifier.padding(start = 16.dp),
-               verticalArrangement = Arrangement.SpaceAround,
+                Modifier
+                    .padding(horizontal = 16.dp)
+                    .fillMaxHeight()
+                    .weight(1f),
+                verticalArrangement = Arrangement.spacedBy(16.dp, Alignment.CenterVertically),
                 horizontalAlignment = Alignment.Start
             ) {
-
-            }
-            Column(
-                verticalArrangement = Arrangement.SpaceAround,
-                horizontalAlignment = Alignment.End
-            ) {
-
+                Text(
+                    text = "Home",
+                    style = textStyle(
+                        size = 20,
+                        color = Color.White,
+                        weight = FontWeight.Medium
+                    )
+                )
+                Text(
+                    text = "Adoptable pets",
+                    style = textStyle(
+                        size = 20,
+                        color = Color.White,
+                        weight = FontWeight.Medium
+                    )
+                )
+                Text(
+                    text = "Chats",
+                    style = textStyle(
+                        size = 20,
+                        color = Color.White,
+                        weight = FontWeight.Medium
+                    )
+                )
+                Text(
+                    text = "Notifications",
+                    style = textStyle(
+                        size = 20,
+                        color = Color.White,
+                        weight = FontWeight.Medium
+                    )
+                )
             }
         }
     }
+}
+
+@Preview
+@Composable
+fun ProfileScreenPreview() {
+    ProfileScreen()
 }
